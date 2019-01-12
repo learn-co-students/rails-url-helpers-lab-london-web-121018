4f97ca4ed60662d6b62f0376da6883b1d10c9e25
github username <github email address>
2019-01-12 17:46:56 +0000

    learn submit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,33 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show,:activate]
+
   def index
     @students = Student.all
   end
 
   def show
+    # @student = Student.find(params[:id])
+    set_student
+    # binding.pry
   end
+
+  def edit
+    @student = set_student
+
+    @student = Student.find(params[:id])
+    # binding.pry
+     @student.active ? @student.update(active:false ) : @student.update(active:true)
+
+
+    # binding.pry
+    redirect_to :action => "show", :id => @student.id
+  end
+
+  def update
+
+  end
+
+
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student1 = Student.create(first_name: "Tom1", last_name: "Jerry1")
+student2 = Student.create(first_name: "Tom2", last_name: "Jerry2")
+student3 = Student.create(first_name: "Tom3", last_name: "Jerry3")
+student4 = Student.create(first_name: "Tom4", last_name: "Jerry4")
+student5 = Student.create(first_name: "Tom5", last_name: "Jerry5")
+student6 = Student.create(first_name: "Tom6", last_name: "Jerry6")
+student7 = Student.create(first_name: "Tom7", last_name: "Jerry7")
